8df84bb6b25e13dae8e715d5d685fa6ca147cf15
PatrykPrusko2019 <patrykprusko@gmail.com>
2021-02-24 13:12:01 +0100

    :

--- app/src/main/java/com/patrykprusko/MainActivity.java
+++ app/src/main/java/com/patrykprusko/MainActivity.java
@@ -1,22 +1,50 @@
 package com.patrykprusko;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 // ca15ad47-b77a-4e16-8f99-9b76227ea9aa -> Google api key
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnPlaySingleVideo = (Button) findViewById(R.id.buttonPlaySingleVideo);
+        Button btnPlaySubmenu = (Button) findViewById(R.id.buttonOpenSubmenu);
+
+        btnPlaySingleVideo.setOnClickListener(this);
+        btnPlaySubmenu.setOnClickListener(this);
 
 
 
 
+    }
 
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = null;
+
+        switch (v.getId()) {
+            case R.id.buttonPlaySingleVideo :
+                intent = new Intent(this, YoutubeActivity.class);
+            break;
+
+            case R.id.buttonOpenSubmenu :
+                intent = new Intent(this, SubmenuActivity.class);
+            break;
+
+            default:
+                //nothing
+        }
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/patrykprusko/SubmenuActivity.java
@@ -0,0 +1,54 @@
+package com.patrykprusko;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+/**
+ *  class submenu
+ */
+public class SubmenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String GOOGLE_API_KEY = "ca15ad47-b77a-4e16-8f99-9b76227ea9aa";
+    private final String YOUTUBE_VIDEO_ID = "HwQBSUD07vQ";
+    private final String YOUTUBE_PLAYLIST = "PLd6x4-VpRx7jixWOSzAvK31_xqXWXG16p";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_submenu);
+
+        Button buttonPlayVideo = (Button) findViewById(R.id.buttonPlayVideo);
+        Button buttonPlayPlaylist = (Button) findViewById(R.id.buttonPlayPlaylist);
+
+        buttonPlayVideo.setOnClickListener(this);
+        buttonPlayPlaylist.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = null;
+
+        switch (v.getId()) {
+
+            case R.id.buttonPlayVideo :
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY, YOUTUBE_VIDEO_ID); // shows player youtube -> video
+                break;
+
+            case R.id.buttonPlayPlaylist :
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST); // shows player youtube -> playlist
+                break;
+        }
+
+        startActivity(intent);
+
+    }
+}
